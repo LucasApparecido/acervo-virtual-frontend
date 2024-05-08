ffba15bc77d39f6e5512b5d9b2a92f53957d35a4
Lucas Apparecido de Oliveira <108204218+LucasApparecido@users.noreply.github.com>
2024-05-08 19:43:56 -0300

    Up primeiras edições em cima do projeto feito pelo professor

---
+++ src/app/artifact/artifact.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-artifact',
+  standalone: true,
+  imports: [],
+  templateUrl: './artifact.component.html',
+  styleUrl: './artifact.component.scss'
+})
+export class ArtifactComponent {
+
+}
