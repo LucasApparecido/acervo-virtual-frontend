6dee9b5291e709b40a836f1c4ddc33d60bb4487c
Lucas Apparecido de Oliveira <108204218+LucasApparecido@users.noreply.github.com>
2024-05-16 13:37:42 -0300

    atualizações de design

--- src/app/artifacts/update/update.component.ts
+++ src/app/artifacts/update/update.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {ActivatedRoute, Router, RouterLink, RouterModule} from "@angular/router";
+import {Artifact} from "../artifact";
+import {ArtifactService} from "../artifact.service";
 
 @Component({
   selector: 'app-update',
   standalone: true,
-  imports: [],
+    imports: [
+      FormsModule,
+      RouterModule,
+      RouterLink
+    ],
   templateUrl: './update.component.html',
   styleUrl: './update.component.scss'
 })
-export class UpdateComponent {
+export class UpdateComponent implements OnInit {
+  artifact: Artifact = new Artifact();
+  title: string = 'Peça x';
 
+  constructor(
+    private activateRouted: ActivatedRoute,
+    private router: Router,
+    private artifactService: ArtifactService
+  ) {
+  }
+
+  ngOnInit(): void {
+    const id = this.activateRouted.snapshot.paramMap.get('id'); // pegar na rota atual o parâmetro especificado na rota
+    console.log("ID edição:" + id + ":");
+    if (id) {
+      this.artifactService.getById(parseInt(id)).subscribe(value => {
+        const artifactAux = value;
+        console.log("INIT FORM:" + JSON.stringify(artifactAux));
+        if (artifactAux) {
+          this.artifact = artifactAux;
+          this.title = 'Alterando peça';
+        }
+      }, error => {
+        console.log("Erro:", JSON.stringify(error));
+        alert(`Erro ao buscar os dados: ${error.error}`);
+      });
+    }
+  }
+
+  onSubmit() {
+    this.artifactService.save(this.artifact)
+      .subscribe(value => {
+        console.log("Salvo:", JSON.stringify(value));
+        this.router.navigate(['/artifacts']);
+      }, error => {
+        console.log("Erro:", JSON.stringify(error));
+        alert('Erro ao salvar: ' + error.error);
+      });
+  }
 }
