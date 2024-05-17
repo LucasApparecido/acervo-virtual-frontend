7d153f92cfd80622cddbc27e2d822728f0bc7d56
Lucas Apparecido de Oliveira <108204218+LucasApparecido@users.noreply.github.com>
2024-05-17 15:03:22 -0300

    correção service front end

--- src/app/artifacts/artifact.service.ts
+++ src/app/artifacts/artifact.service.ts
@@ -15,28 +15,28 @@ export class ArtifactService {
 
   getAll(): Observable<Artifact[]> {
     console.log('Inicio getAll');
-    return this.http.get<Artifact[]>('http://localhost:8080/api/v1/artifact');
+    return this.http.get<Artifact[]>('http://localhost:8080/api/v1/artefacts');
   }
 
   getById(id: number): Observable<Artifact> {
     console.log('Inicio getAll');
-    return this.http.get<Artifact>(`http://localhost:8080/api/v1/artifact/${id}`);
+    return this.http.get<Artifact>(`http://localhost:8080/api/v1/artefacts/${id}`);
   }
 
   save(artifact: Artifact): Observable<Artifact> {
     let response : Observable<Artifact>;
     if (artifact.artifactId) {
       console.log('Alterar:' + JSON.stringify(artifact));
-      response = this.http.put<Artifact>(`http://localhost:8080/api/v1/artifact/${artifact.artifactId}`, artifact);
+      response = this.http.put<Artifact>(`http://localhost:8080/api/v1/artefacts/${artifact.artifactId}`, artifact);
     } else {
       console.log('Incluir:' + JSON.stringify(artifact));
-      response = this.http.post<Artifact>('http://localhost:8080/api/v1/artifact', artifact);
+      response = this.http.post<Artifact>('http://localhost:8080/api/v1/artefacts', artifact);
     }
     return response;
   }
 
   delete(id: number): Observable<Artifact> {
     console.log('Alterar:' + id);
-    return this.http.delete<Artifact>(`http://localhost:8080/api/v1/artifact/${id}`);
+    return this.http.delete<Artifact>(`http://localhost:8080/api/v1/artefacts/${id}`);
   }
 }
